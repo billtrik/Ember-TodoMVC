ab470bfb76a6561020e310d108e450886fc86937
Bill Trikalinos <billtrik@gmail.com>
2013-12-15 12:12:10 +0200

    Created the API/v1/todos controller - route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Cultural::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :todos
+      root 'todos#index'
+    end
+  end
+
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
